6effc293c068a55594811d013c66e834a58cc4a6
pyoseunghoon <55734481+pyoseunghoon@users.noreply.github.com>
2025-04-18 00:43:43 +0900

    updateCart 정의

--- src/advanced/stores/useSumStore.ts
+++ src/advanced/stores/useSumStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand/react';
+import { CartItem } from './useCartStore';
 
 // 고객 구매 정보 상태 정의
 interface SumState {
@@ -16,6 +17,22 @@ interface SumState {
   updateCart: (items: { id: string; price: number; quantity: number }[]) => void;
 }
 
+const MIN_DISCOUNT_QUANTITY = 10;
+
+const BULK_DISCOUNT_ITEM_COUNT = 30;
+const BULK_DISCOUNT_RATE = 0.25;
+
+const TUESDAY_DISCOUNT_RATE = 0.1;
+
+// 상품별 할인율
+const DISCOUNT_RATES = {
+  p1: 0.1,
+  p2: 0.15,
+  p3: 0.2,
+  p4: 0.05,
+  p5: 0.25,
+};
+
 export const useSumStore = create<SumState>((set) => ({
   total: 0,
   originTotal: 0,
@@ -28,4 +45,53 @@ export const useSumStore = create<SumState>((set) => ({
   setItemCnt: (val) => set({ itemCnt: val }),
   setDiscountRate: (val) => set({ discountRate: val }),
   setBonusPts: (val) => set({ bonusPts: val }),
+  updateCart: (items: CartItem[]) => {
+    let total = 0;
+    let originTotal = 0;
+    let itemCnt = 0;
+    let discountRate = 0;
+
+    for (const item of items) {
+      const itemTotal = item.price * item.quantity;
+      originTotal += itemTotal;
+      itemCnt += item.quantity;
+
+      let discount = 0;
+      if (item.quantity >= MIN_DISCOUNT_QUANTITY) {
+        discount = DISCOUNT_RATES[item.id] || 0;
+      }
+
+      total += itemTotal * (1 - discount);
+    }
+
+    if (itemCnt >= BULK_DISCOUNT_ITEM_COUNT) {
+      const bulkDiscountPrice = originTotal * BULK_DISCOUNT_RATE;
+      const itemDiscountPrice = originTotal - total;
+
+      if (bulkDiscountPrice > itemDiscountPrice) {
+        total = originTotal * (1 - BULK_DISCOUNT_RATE);
+        discountRate = BULK_DISCOUNT_RATE;
+      } else {
+        discountRate = itemDiscountPrice / originTotal;
+      }
+    } else {
+      discountRate = (originTotal - total) / originTotal;
+    }
+
+    const TUESDAY = 2;
+    if (new Date().getDay() === TUESDAY) {
+      total *= 1 - TUESDAY_DISCOUNT_RATE;
+      discountRate = Math.max(discountRate, TUESDAY_DISCOUNT_RATE);
+    }
+
+    const bonusPts = Math.floor(total / 1000);
+
+    set({
+      total,
+      originTotal,
+      itemCnt,
+      discountRate,
+      bonusPts,
+    });
+  },
 }));
