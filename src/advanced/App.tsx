64d52034532c06ca753769188437310d892be0c1
pyoseunghoon <55734481+pyoseunghoon@users.noreply.github.com>
2025-04-18 00:55:19 +0900

    모든 ui 컴포넌트 app에 추가

--- src/advanced/App.tsx
+++ src/advanced/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ProductSelect from './ui/ProductSelect';
 import SumView from './ui/SumView';
+import StockInfoView from './ui/StockInfoView';
+import CartList from './ui/CartList';
 
 const App = () => {
   return (
     <div className="bg-gray-100 p-8">
       <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8">
         <h1 className="text-2xl font-bold mb-4">장바구니</h1>
+        <CartList />
         <SumView />
         <ProductSelect />
+        <StockInfoView />
       </div>
     </div>
   );
