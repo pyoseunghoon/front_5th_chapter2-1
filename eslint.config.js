cc044f539273cc7c0487dac417de904de34efcbb
pyoseunghoon <55734481+pyoseunghoon@users.noreply.github.com>
2025-04-17 22:39:22 +0900

    eslint.config, vite.config react 환경에 추가에 따라 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,35 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
+  // 공통 무시 파일 설정
+  { ignores: ['dist', 'node_modules', 'README.md', 'package.json', 'pnpm-lock.yaml', '.github/'] },
+
+  // TypeScript + React 설정 (.ts, .tsx)
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+
+  // JavaScript 설정 (.js)
   {
     files: ['**/*.js'],
-    ignores: ['node_modules', 'dist', 'README.md', 'package.json', 'pnpm-lock.yaml', '.github/'],
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: 'module',
@@ -15,4 +41,4 @@ export default [
       'prettier/prettier': 'error',
     },
   },
-];
+);
